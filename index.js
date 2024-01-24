7ba3d9158db99648054551c49934f8417e2962d2
Rater7tara <nowshinakteremu005@gmail.com>
2024-01-24 09:47:34 +0600

    post put delete added

--- index.js
+++ index.js
@@ -30,8 +30,57 @@ async function run() {
     const houseUserCollection = client.db('houseRental').collection('houseUser');
     const houseCollection = client.db('houseRental').collection('houseDB');
 
+    
+    app.get('/houseDB/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await houseCollection.findOne(query);
+        res.send(result);
+    })
+  
+  
+  
+      app.post('/houseDB', async(req, res) =>{
+        const newHouse = req.body;
+        console.log(newHouse);
+        const result = await houseCollection.insertOne(newHouse)
+        res.send(result);
+      })
+  
+      app.delete('/houseDB/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await houseCollection.deleteOne(query);
+        res.send(result);
+    })
+  
+    app.put('/houseDB/:id', async(req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true };
+      const updateHouse =req.body;
+      const house = {
+        $set:{
+          name: updateHouse.name,
+          address: updateHouse.address,
+          city: updateHouse.city,
+          bedroom: updateHouse.bedroom,
+          room: updateHouse.room,
+          rent: updateHouse.rent,
+          phone: updateHouse.phone,
+          avaible: updateHouse.avaible,
+          details: updateHouse.details,
+          picture: updateHouse.picture
+        }
+      }
+  
+      const result = await houseCollection.updateOne(filter, house, options)
+      res.send(result);
+    })
+  
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
